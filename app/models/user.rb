4539e889c7381e9382ac9a17b49c585ed7daf0c0
Walmir Neto <wfsneto@gmail.com>
2012-04-29 20:57:01 -0300

    ajustes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
-  after_create :welcome_email 
+  # after_create :welcome_email 
   
   has_many :links
   # has_many :categories, through: :links
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
@@ -15,13 +15,8 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email, :username
   
   validates_presence_of :username
-  
-  # def by_username(username)
-  #   where('username = :username', :username => "%#{username}%")
-  # end
 
-
-  private
+  protected
     def welcome_email
       UserMailer.welcome_email(self)
     end
