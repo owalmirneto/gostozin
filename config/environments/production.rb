2419a89d09088b79b933dd87172e9fd950a82e26
Walmir Neto <wfsneto@gmail.com>
2012-04-29 21:43:51 -0300

    configuração smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,16 @@ Gostozin::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # configuration for send mail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'wfsneto.com.br',
+    :user_name            => 'mail@wfsneto.com.br',
+    :password             => 's3nh49Y4bYJ>rf4c1l',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 end
