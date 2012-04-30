90fe9ca4900a843690fcc28cca4fb9d55277865a
Walmir Neto <wfsneto@gmail.com>
2012-04-29 21:01:00 -0300

    ajustes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
-  # after_create :welcome_email 
+  after_create :welcome_email
   
   has_many :links
   # has_many :categories, through: :links
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
